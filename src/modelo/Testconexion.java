cb41d275ab13ea654c3beccd64efc7f0f9eff87e
pablo <pablo@ORDENADORPABLO>
2020-04-30 10:46:31 +0200

    primer commit en el que he creado el modelo vista controlador , con su main , su primera pantalla de momento no hace nada pero es para probar que sale , una clae donante , test conexion con la base de datos , el archivo datos.ini , la controladora y el fxml

---
+++ src/modelo/Testconexion.java
@@ -0,0 +1,64 @@
+package modelo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class Testconexion {
+
+
+	private String user= "";
+	private String url= "";
+	private String usr = "";
+	private String pwd = "";
+	private Connection conexion;
+	
+
+	public Testconexion()  {
+		Properties propiedades = new Properties();
+		InputStream entrada = null;
+		try {
+			File miFichero = new File("src/Modelo/datos.ini");
+			if (miFichero.exists()){
+				entrada = new FileInputStream(miFichero);
+				propiedades.load(entrada);
+				url=propiedades.getProperty("url");
+				user=propiedades.getProperty("user");
+				pwd=propiedades.getProperty("pwd");
+				usr=propiedades.getProperty("usr");
+			}
+
+			else
+				System.out.println("Fichero no encontrado");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}finally {
+			if (entrada != null) {
+				try {
+					entrada.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conexion = DriverManager.getConnection(url, usr, pwd);
+
+			if(conexion.isClosed())
+				System.out.println("Fallo en Conexión con la Base de Datos");
+
+
+		}catch (Exception e) {
+			System.out.println("ERROR en conexión con ORACLE");
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
---
+++ src/vista/controladorasangre.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class controladorasangre {
+	@FXML
+	private Button btndonante;
+	@FXML
+	private Button btndonacion;
+	@FXML
+	private Button btninformes;
+	
+}
